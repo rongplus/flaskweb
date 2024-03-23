cc36238d4397d8da16a2cf549e334e9eac3ca3ea
rongplus <rong.rh.plus@gmail.com>
2024-03-22 21:43:46 -0400

    added openai and ollama

--- testapi.py
+++ testapi.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import markdown.extensions.fenced_code
 #pip install Flask-Markdown
+from openai import OpenAI
+from ollama import generate
+from ollama import AsyncClient
+
 
 def test():
     if request.method == "POST": 
@@ -47,3 +51,41 @@ def template2():
     #<li> {{content|safe}}</li><br>
     #<li> {{content|markdown }}</li><br>
     return render_template('template.html', content=md_template_string )
+
+
+
+def tellJoke():
+    response = generate('llama2', 'Tell me a funny joke')
+    print(response['response'])
+    return render_template("joke.html",joke=response['response'])
+
+def tellJoke1():
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    temperature = 0.2,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Tell me a joke."}
+    ]
+    )
+
+    #print(completion.choices[0].message)
+    a = completion.choices[0].message.content.split('\n') 
+
+    return render_template("joke.html",joke=a)
+
+def askQuestion():
+    question = ''
+    if request.method == "POST":   
+        question = request.form.get("question")
+    if question:
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question}
+        ]
+        )
+        return render_template("askme.html",joke=completion.choices[0].message.content)
+    
+    return render_template("askme.html")
